90695ff233e0fa9c1e1c0f80ac2c01c06befce5c
Adam Zivlak <adam.zivlak@gmail.com>
2024-05-22 15:10:08 +0200

    Lagring av användaruppgifter i JSON-fil

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,15 +1,46 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace WebApplication1testingRazor.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public void OnGet()
+        {
+        }
+
         public IActionResult OnPost()
         {
-            // LÄgg till autentiseringslogik
-            // Omdirigera nu direkt till Privacy-sidan
-            return RedirectToPage("/Privacy");
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "users.json");
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                var users = JsonConvert.DeserializeObject<List<User>>(jsonData);
+
+                var matchingUser = users?.FirstOrDefault(user => user.Username == Username && user.Password == Password);
+                if (matchingUser != null)
+                {
+                    // Korrekt inmatning
+                    return RedirectToPage("/Privacy");
+                }
+
+                // Fel inmatning
+                ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                return Page();
+            }
+
+            // Hantera fall där användarnamn eller lösenord är tomma
+            ModelState.AddModelError(string.Empty, "Username and Password are required.");
+            return Page();
         }
     }
 }
