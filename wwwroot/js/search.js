c30d0516c338acb504a50c730dd63ed47d6f8808
Rebecca Eskekärr <Rebecca.eskekarr@gmail.com>
2024-05-23 15:17:44 +0200

    Transfered script to seperate files, no functionality yet in filter

---
+++ wwwroot/js/search.js
@@ -0,0 +1,21 @@
+﻿function searchLocation() {
+    var searchTerm = document.getElementById('searchInput').value;
+    var url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + searchTerm;
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function (data) {
+            if (data && data.length > 0) {
+                var lat = parseFloat(data[0].lat);
+                var lon = parseFloat(data[0].lon);
+                map.setView([lat, lon], 13);
+            } else {
+                alert('Location not found');
+            }
+        },
+        error: function () {
+            alert('Error occurred while searching for location');
+        }
+    });
+}
