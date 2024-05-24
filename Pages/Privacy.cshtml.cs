5caf8ad07a7a4fe0b8803af7d7d432cfb73d9438
Adam Zivlak <adam.zivlak@gmail.com>
2024-05-24 10:42:47 +0200

    Save pin to json

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using OsmSharp.API;
 using System.Globalization;
 
 namespace WebApplication1testingRazor.Pages
@@ -12,6 +14,7 @@ namespace WebApplication1testingRazor.Pages
         public string PinTitle { get; private set; }
         public string PinDescription { get; private set; }
 
+
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
@@ -22,13 +25,39 @@ namespace WebApplication1testingRazor.Pages
         }
 
         //POST method for receiving pin data.
-        public IActionResult OnPost(string owner, string latitude, string longitude, string title, string description)
+        public IActionResult OnPost(string owner, string latitude, string longitude, string title, string description, string imageLink)
         {
             double lat = Convert.ToDouble(latitude, CultureInfo.InvariantCulture);
             double lon = Convert.ToDouble(longitude, CultureInfo.InvariantCulture);
 
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "pinLocations.json");
+
+            List<MapPin> mapPins = new List<MapPin>();
+            
             MapPin mapPin = new MapPin(owner, lat, lon, title, description, "placeholder_imagelink");
-            Console.WriteLine($"{mapPin}");
+            // Console.WriteLine($"{mapPin}");
+
+            string json = JsonConvert.SerializeObject(mapPins, Formatting.Indented);
+
+            System.IO.File.WriteAllText(filePath, json );
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                mapPins = JsonConvert.DeserializeObject<List<MapPin>>(jsonData) ?? new List<MapPin>();
+            }
+
+            foreach (MapPin test in mapPins)
+            {
+                Console.WriteLine(test);
+            }
+
+            mapPins.Add(new MapPin(owner,lat,lon,title,description,imageLink));
+
+            string newJsonData = JsonConvert.SerializeObject(mapPins, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, newJsonData);
+
+            // Console.WriteLine("MapPin data written succesfully!");
 
             //_logger.LogInformation("Received coordinates: Latitude = {Latitude}, Longitude = {Longitude}", latitude, longitude);
 
