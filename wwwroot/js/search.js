2916985a5512d4e468a729201ab56abce2036d78
Rebecca Eskekärr <rebecca@eskekarr.se>
2024-05-27 14:40:49 +0200

    Functionality in search, and adjustable zoom

--- wwwroot/js/search.js
+++ wwwroot/js/search.js
@@ -1,52 +1,32 @@
-﻿//function searchLocation() {
-//    var searchTerm = document.getElementById('searchInput').value;
-//    var url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + searchTerm;
-
-//    $.ajax({
-//        url: url,
-//        type: 'GET',
-//        success: function (data) {
-//            if (data && data.length > 0) {
-//                var lat = parseFloat(data[0].lat);
-//                var lon = parseFloat(data[0].lon);
-//                map.setView([lat, lon], 13);
-//            } else {
-//                alert('Location not found');
-//            }
-//        },
-//        error: function () {
-//            alert('Error occurred while searching for location');
-//        }
-//    });
-//}
-function searchLocation() {
-    var category = $("#categoryDropdown").val();
-    var query = $("#searchInput").val();
+﻿function searchLocation() {
+    var searchTerm = document.getElementById('searchInput').value;
+    var url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + searchTerm;
 
     $.ajax({
-        url: `/api/location/search?category=${category}&query=${query}`,
-        method: "GET",
-        success: function (data) {
-            // Clear existing markers
-            map.eachLayer(function (layer) {
-                if (layer instanceof L.Marker) {
-                    map.removeLayer(layer);
+        url: url,
+        type: 'GET',
+       success: function (data) {
+            if (data && data.length > 0) {
+                var lat = parseFloat(data[0].lat);
+                var lon = parseFloat(data[0].lon);
+                map.setView([lat, lon], 13);
+                // Check if bounding box data is available
+                if (data[0].boundingbox) {
+                    var bbox = data[0].boundingbox.map(Number);
+                    var southWest = L.latLng(bbox[0], bbox[2]);
+                    var northEast = L.latLng(bbox[1], bbox[3]);
+                    var bounds = L.latLngBounds(southWest, northEast);
+                    map.fitBounds(bounds);
+                } else {
+                    // Default to a zoom level of 13 if bounding box is not available
+                    map.setView([lat, lon], 13);
                 }
-            });
-
-            // Add new markers
-            data.locations.forEach(location => {
-                L.marker([location.lat, location.lng]).addTo(map)
-                    .bindPopup(`<b>${location.name}</b><br>${location.type}`);
-            });
-
-            // Center map to the first location
-            if (data.locations.length > 0) {
-                map.setView([data.locations[0].lat, data.locations[0].lng], 13);
+            } else {
+                alert('Location not found');
             }
         },
-        error: function (xhr, status, error) {
-            console.error("Error fetching location data:", error);
+        error: function () {
+            alert('Error occurred while searching for location');
         }
-    });
+   });
 }
