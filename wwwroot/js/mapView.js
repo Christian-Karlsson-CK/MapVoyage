cd545b2ec41556d2d015efa4fcb419541ea120e3
MStameus <morris.stameus@hotmail.se>
2024-05-29 11:00:08 +0200

    moved script from mapview fiel to privacy cshtml and it seeminglty now loads and creates puins correcty

--- wwwroot/js/mapView.js
+++ wwwroot/js/mapView.js
@@ -36,60 +36,13 @@ document.getElementById('addPinButton').addEventListener('click', function () {
 
 
 
-document.getElementById('savePin').addEventListener('click', function () {
-
-    var title = document.getElementById('pinTitle').value;
-    var description = document.getElementById('pinDescription').value;
-    var imageInput = document.getElementById('pinImage');
-    var imageFile = imageInput.files[0];
-    var username = document.getElementById('currentUser').getAttribute('dataUsername');
-
-    var newPin = {
-        Owner: username,
-        Latitude: lat,
-        Longitude: lng,
-        Title: title,
-        Description: description,
-    };
-
-    var marker = L.marker([lat, lng]).addTo(map)
-
-    if (imageFile) {
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            var imageUrl = event.target.result;
-            marker.on('click', function () {
-                document.getElementById('info').innerHTML = '<b>Created by: ' + newPin.Owner + '</br>Title: ' + title + '</b><br>Description: ' + description + '<br><img src="' + imageUrl + '" alt="' + title + '" style="max-width: 100%; height: auto;">';
-            });
-        };
-
-        reader.readAsDataURL(imageFile);
-    } else {
-        marker.on('click', function () {
-            document.getElementById('info').innerHTML = '<b>Created by: ' + newPin.Owner + '</br>Title: ' + title + '</b><br>Description: ' + description;
-        });
-    }
-
-    //Ajax POST request to send new pin data
-    $.ajax({
-        url: '/Privacy',
-        type: 'POST',
-        dataType: 'text',
-        contentType: "application/json",
-        data: JSON.stringify(newPin)
-    });
-
-    var pinForm = document.getElementById('pinCreateForm');
-    pinForm.style.display = 'none';
-    clearForm();
-    addingPin = false;
-});
 document.getElementById('cancelPin').addEventListener('click', function () {
     var pinForm = document.getElementById('pinCreateForm');
     clearForm();
     pinForm.style.display = 'none';
     addingPin = false;
 });
+
 
 function removePin(lat, lng) {
     var pinToRemove = {
